a1e9f129ec1ea51519bc24e102936ccdde46764e
mgwebb <mark.webb@cgi.com>
2018-05-31 16:44:08 -0600

    added org level processing

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 
 // AppInfo is a struct representing the output seen on screen.
 type AppInfo struct {
+	Space     string
 	Name      string
 	Instances string
 	Memory    string
@@ -42,12 +43,14 @@ func (c *FilterApps) Run(cliConnection plugin.CliConnection, args []string) {
 		trace.NewLogger(os.Stdout, false, "false", ""),
 	)
 	c.UI = ui
-	var apps []plugin_models.GetAppsModel
 	all := true
+	allorg := false
 
-	/* check for proper usage */
-	if len(args) > 2 ||
-		(len(args) == 2 && args[1] != "-a") {
+	//c.UI.Say(terminal.FailureColor("\nargs len = " + strconv.Itoa(len(args))))
+
+	invalidargs := CheckProperUsage(args)
+
+	if invalidargs == true {
 		c.UI.Say(terminal.FailureColor("\nError - invalid usage.\n"))
 		_, err := cliConnection.CliCommand("help", "started-apps")
 		if err != nil {
@@ -56,43 +59,72 @@ func (c *FilterApps) Run(cliConnection plugin.CliConnection, args []string) {
 		}
 		return
 	}
-
-	/* check for excluded flag */
-	if len(args) == 2 && args[1] == "-a" {
-		all = true
-	} else {
+	/*
+	  We've already checked for correct number of args and valid values,
+	  so now just determine what user is asking for.
+	*/
+	if len(args) == 2 {
+		if args[1] == "-a" {
+			all = true
+			allorg = false
+		} else if args[1] == "-o" {
+			all = false
+			allorg = true
+		}
+	} else if len(args) == 1 {
 		all = false
+		allorg = false
+	} else {
+		all = true
+		allorg = true
 	}
 
-	username, _ := c.Connection.Username()
-	var org, space string
+	//TODO - remove these once testing is completed.
+	//c.UI.Say(terminal.FailureColor("all = " + strconv.FormatBool(all)))
+	//c.UI.Say(terminal.FailureColor("allorg = " + strconv.FormatBool(allorg)))
+
+	var org string
 	var currentOrg plugin_models.Organization
 	var currentSpace plugin_models.Space
 	currentOrg, err = c.Connection.GetCurrentOrg()
+	username, _ := c.Connection.Username()
 	if err == nil {
 		org = currentOrg.OrganizationFields.Name
 	}
 	currentSpace, err = c.Connection.GetCurrentSpace()
-	if err == nil {
-		space = currentSpace.Name
-	}
-	c.UI.Say("\nGetting apps in org " + terminal.PromptColor(org) + " / space " + terminal.PromptColor(space) + " as " + terminal.PromptColor(username) + "...")
 
-	apps, err = c.Connection.GetApps()
-
+	// Check for error, then whether we actually find a targeted space
 	if err != nil {
 		c.UI.Say(terminal.FailureColor("FAILED"))
 		c.UI.Say(err.Error())
+	} else if len(currentSpace.Guid) < 1 {
+		c.UI.Say(terminal.FailureColor("FAILED"))
+		c.UI.Say("\nNo space targeted, use " + terminal.PromptColor("'cf target -s'") + "to target a space.\n")
 		return
 	}
 
-	appInfo := RetrieveApps(apps, all)
+	if allorg == true {
+		RetrieveOrgApps(all, c, org, username, currentSpace)
+	} else {
+		RetrieveSpaceApps(all, c, org, username, currentSpace)
+	}
 
-	c.UI.Say("OK\n")
+}
 
+//PrintApps will iterate through a list of apps and print in same format as 'cf apps' command.
+//NOTE - this will be common for printing apps for a single space or for all spaces in an org.  Keep track of the
+//change in "space" to print the header.
+func PrintApps(appInfo []AppInfo, c *FilterApps, org string, username string, spaceName string) {
+	// always print the header
 	if len(appInfo) > 0 {
+		var currentSpaceName = ""
 		table := c.UI.Table([]string{"name", "requested state", "instances", "memory", "disk", "urls"})
 		for _, app := range appInfo {
+			if currentSpaceName != app.Space {
+				currentSpaceName = app.Space //reset the var that holds the current space name
+				c.UI.Say("Getting apps in org " + terminal.PromptColor(org) + " / space " + terminal.PromptColor(currentSpaceName) + " as " + terminal.PromptColor(username) + "...")
+				c.UI.Say(terminal.SuccessColor("OK\n"))
+			}
 			if app.State == "started" {
 				table.Add(app.Name, app.State, app.Instances, app.Memory, app.Disk, app.Urls)
 			} else {
@@ -104,13 +136,87 @@ func (c *FilterApps) Run(cliConnection plugin.CliConnection, args []string) {
 		table.Print()
 		c.UI.Say("\n")
 	} else {
+		c.UI.Say("Getting apps in org " + terminal.PromptColor(org) + " / space " + terminal.PromptColor(spaceName) + " as " + terminal.PromptColor(username) + "...")
+		c.UI.Say(terminal.SuccessColor("OK\n"))
 		fmt.Printf("No %s apps found", terminal.PromptColor("started"))
-		fmt.Println("\n")
+		fmt.Println("")
 	}
+
 }
 
-// RetrieveApps function iterates through the results of the GetApps() function and creates an AppInfo array.
-func RetrieveApps(apps []plugin_models.GetAppsModel, all bool) (appInfo []AppInfo) {
+// CheckProperUsage function ensures the proper syntax for invoking this plugin
+func CheckProperUsage(args []string) (invalidargs bool) {
+	if len(args) > 3 {
+		invalidargs = true
+	}
+	if len(args) == 2 && (args[1] != "-a" && args[1] != "-o") {
+		invalidargs = true
+	}
+	if len(args) == 3 {
+		if (args[1] == "-a" && args[2] == "-o") ||
+			(args[1] == "-o" && args[2] == "-a") {
+			invalidargs = false
+		} else {
+			invalidargs = true
+		}
+	}
+	return invalidargs
+}
+
+//RetrieveSpaceApps handles a request for showing apps in only the current space
+func RetrieveSpaceApps(all bool, c *FilterApps,
+	org string, username string, currentSpace plugin_models.Space) {
+
+	var err error
+	var apps []plugin_models.GetAppsModel
+
+	apps, err = c.Connection.GetApps()
+	if err != nil {
+		c.UI.Say(terminal.FailureColor("FAILED"))
+		c.UI.Say(err.Error())
+		return
+	}
+	var appInfo = BuildAppInfo(apps, all, currentSpace.Name)
+
+	//now that we have our array, let's print...
+	PrintApps(appInfo, c, org, username, currentSpace.Name)
+
+}
+
+// RetrieveOrgApps function iterates throught the spaces in an org,
+//   then gets the apps in each space,  then call new print PrintOrgApps method to display.
+func RetrieveOrgApps(all bool, c *FilterApps, org string, username string,
+	currentSpace plugin_models.Space) {
+
+	var err error
+	var apps []plugin_models.GetAppsModel
+	var spaceList []plugin_models.GetSpaces_Model
+	var appInfo []AppInfo
+
+	//Get the list of spaces in the org
+	spaceList, err = c.Connection.GetSpaces()
+	if err != nil {
+		c.UI.Say(terminal.FailureColor("FAILED"))
+		c.UI.Say(err.Error())
+		return
+	}
+	//Iterate through spaces and print apps for each
+	for _, space := range spaceList {
+		c.Connection.CliCommandWithoutTerminalOutput("target", "-s", space.Name) //switch to the next space
+		apps, err = c.Connection.GetApps()                                       //get the list of apps in this space
+		appInfo = BuildAppInfo(apps, all, space.Name)
+		PrintApps(appInfo, c, org, username, space.Name) // pass the space.Name since the appInfo array may be empty if there are no apps
+		c.UI.Say("\n")                                   //print an extra empty line to make it easier to read
+	}
+
+	//Now switch back to the original space they were in
+
+	c.Connection.CliCommandWithoutTerminalOutput("target", "-s", currentSpace.Name)
+
+}
+
+// BuildAppInfo - takes a []plugin_models.GetAppsModel and builds an AppInfo array
+func BuildAppInfo(apps []plugin_models.GetAppsModel, all bool, spaceName string) (appInfo []AppInfo) {
 	appInfo = make([]AppInfo, 0, 50)
 	for _, app := range apps {
 		if app.State == "started" || all == true {
@@ -120,7 +226,7 @@ func RetrieveApps(apps []plugin_models.GetAppsModel, all bool) (appInfo []AppInf
 				u = append(u, r.Host+"."+r.Domain.Name)
 			}
 			inst := strconv.Itoa(app.RunningInstances) + "/" + strconv.Itoa(app.TotalInstances)
-			a := AppInfo{Name: app.Name, Instances: inst, Memory: ConvertSize(app.Memory), Disk: ConvertSize(app.DiskQuota), Urls: strings.Join(u, ", "), State: app.State}
+			a := AppInfo{Space: spaceName, Name: app.Name, Instances: inst, Memory: ConvertSize(app.Memory), Disk: ConvertSize(app.DiskQuota), Urls: strings.Join(u, ", "), State: app.State}
 			appInfo = append(appInfo, a)
 		}
 	}
@@ -157,6 +263,7 @@ func (c *FilterApps) GetMetadata() plugin.PluginMetadata {
 					Usage: "cf started-apps",
 					Options: map[string]string{
 						"a": "Include stopped apps",
+						"o": "Show apps for entire org",
 					},
 				},
 			},
